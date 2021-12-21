6277d3f3083a2ed1ae3a270e14adb341b4f16447
maba-dev <cherif.ba@hotmail.fr>
2021-12-21 12:54:46 +0100

    task 9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""a function that prints the last digit of a number."""
+
+
+def print_last_digit(number):
+    """print the last digit of a number"""
+    i = number % 10
+    return (i)

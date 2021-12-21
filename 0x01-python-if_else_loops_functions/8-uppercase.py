e53cdb698a9cdca1c8e580a0e1fce13279504a4f
maba-dev <cherif.ba@hotmail.fr>
2021-12-21 16:53:51 +0100

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" a function that prints a string in uppercase followed by a new line."""
+
+
+def uppercase(str):
+    for i in (str):
+        i = ord(i)
+        if (i >= 97 and i <= 122):
+            i = i - 32
+            print("{:c}".format(i), end="")
+        else:
+            print("{:c}".format(i), end="")
+    print()

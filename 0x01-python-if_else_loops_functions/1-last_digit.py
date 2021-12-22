ffd3d3cbc6e3faf63a4bf145bce13d72ef7a88e2
maba-dev <cherif.ba@hotmail.fr>
2021-12-22 12:07:48 +0100

    task 1 retry

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-
 if number < 0:
     modulo = number % -10
 else:
@@ -10,6 +9,6 @@ print("Last digit of {:d} is {:d} ".format(number, modulo), end="")
 if modulo > 5:
     print("and is greater than 5")
 elif modulo == 0:
-    print("is 0 and is 0")
+    print("and is 0")
 else:
     print("and is less than 6 and not 0")

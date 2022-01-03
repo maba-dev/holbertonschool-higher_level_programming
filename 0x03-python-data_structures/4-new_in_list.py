d84d343c7c1780020c8454345b0dcf275b365025
maba-dev <cherif.ba@hotmail.fr>
2022-01-03 14:42:59 +0100

    task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,12 +3,11 @@
 
 
 def new_in_list(my_list, idx, element):
-    if my_list:
 
-        if idx < 0:
-            return my_list
-        if idx > len(my_list):
-            return my_list
-        new_in_list = list(my_list)
-        my_list[idx] = element
-        return new_in_list
+    if idx < 0:
+        return my_list
+    if idx > len(my_list):
+        return my_list
+    new_in_list = list(my_list)
+    my_list[idx] = element
+    return new_in_list

122862f3f7d60be4b7eeb955b7685fec475ae8ab
maba-dev <cherif.ba@hotmail.fr>
2022-01-03 21:22:52 +0100

    task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -7,6 +7,6 @@ def max_integer(my_list=[]):
         return None
     max_integer = 0
     for i in my_list:
-        if i >= max_integer:
+        if i > max_integer:
             max_integer = i
     return max_integer

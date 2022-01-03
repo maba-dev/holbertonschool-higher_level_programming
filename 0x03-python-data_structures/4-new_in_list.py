e0a8aa3d5e48ed1fa37703d16368879ce3cd05f2
maba-dev <cherif.ba@hotmail.fr>
2022-01-03 15:13:10 +0100

    task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -9,5 +9,5 @@ def new_in_list(my_list, idx, element):
     if idx > len(my_list):
         return my_list
     my_new_in_list = my_list.copy()
-    my_list[idx] = element
+    my_new_in_list[idx] = element
     return my_new_in_list

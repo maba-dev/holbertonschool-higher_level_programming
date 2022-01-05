af170fccfe237b3ac24e60c07337845fe766bb8e
maba-dev <cherif.ba@hotmail.fr>
2022-01-05 20:06:13 +0100

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,4 +5,6 @@
 def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict):
         return None
+    if len(a_dictionary) == 0:
+        return None
     return (max(a_dictionary, key=a_dictionary.get))

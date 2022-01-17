c0ae546a96b806b492c8e6c4b37da25cb459eddf
maba-dev <cherif.ba@hotmail.fr>
2022-01-17 15:48:03 +0100

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,10 +3,12 @@
 
 
 def safe_print_list(my_list=[], x=0):
+    i = 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end='')
+            i += 1
         except IndexError:
             break
     print()
-    return x
+    return i

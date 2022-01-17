11224daf369b6119eff4b3fce4ec41f539089c39
maba-dev <cherif.ba@hotmail.fr>
2022-01-17 11:39:59 +0100

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" a function that prints x elements of a list."""
+
+
+def safe_print_list(my_list=[], x=0):  
+    for i in range(x):
+            try:
+                print("{:d}".format(my_list[i]), end='')
+            except IndexError:
+                break
+    print()
+    return x

3c0c76a2112bac5af894e8f9b46b956ac17a9433
maba-dev <cherif.ba@hotmail.fr>
2022-01-18 17:04:55 +0100

    task 4 retry pep8

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,7 +3,9 @@
 
 
 class Square:
-    def __init__(self, size = 0):
+    """ Define class"""
+    def __init__(self, size=0):
+        """ Define constructor"""
         self.size = size
 
     @property
@@ -17,7 +19,6 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
 
     def area(self):
         return self.__size * self.__size

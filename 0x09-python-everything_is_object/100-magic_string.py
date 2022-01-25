ddadb03740ef990f04b136552a7fdff13ef9594f
maba-dev <cherif.ba@hotmail.fr>
2022-01-25 19:00:23 +0100

    task 100

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-""" a function magic_string() that returns a string “BestSchool” n times"""
-
-
 def magic_string(string=[]):
     string.append("BestSchool")
     return ", ".join(string)

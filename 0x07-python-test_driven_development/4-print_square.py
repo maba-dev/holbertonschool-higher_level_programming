e022e10c8f216f04f60815255990ef216ca44b8a
maba-dev <cherif.ba@hotmail.fr>
2022-01-26 14:38:31 +0100

    task 4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -5,7 +5,7 @@
 def print_square(size):
     """ define a function that prints a square"""
     if not isinstance(size, int):
-        raise TypeError("size must be integer")
+        raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
     for i in range(0, size):

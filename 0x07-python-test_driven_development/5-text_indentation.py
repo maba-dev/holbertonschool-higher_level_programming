77c4b4f9f1961476a032d91ca4701d6c4cafbce9
maba-dev <cherif.ba@hotmail.fr>
2022-01-26 15:56:33 +0100

    task 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,11 +3,18 @@
 
 
 def text_indentation(text):
+    """ a function that prints text """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     string = ""
     for i in range(0, len(text)):
         string += text[i]
         if text[i] == "." or text[i] == "?" or text[i] == ":":
-            string += "\n \n"
-    print("{:s}\n".format(string))
+            string += "\n\n"
+    string_last = ""
+    for j in range(0, len(string)):
+        if (string[j - 1] == '\n') and string[j] == ' ':
+            continue
+        else:
+            string_last += string[j]
+    print("{:s}".format(string_last), end="")

39a74157a3cb055ecb11763d9d2219866d730957
maba-dev <cherif.ba@hotmail.fr>
2022-01-31 21:13:23 +0100

    task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" a class Square that inherits from Rectangle (9-rectangle.py):"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        self.size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)

675c451df03706f8706e8dc95aec91d9c8640261
maba-dev <cherif.ba@hotmail.fr>
2022-01-31 15:38:42 +0100

    task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3 
+"""a function that returns True if the object is an instance of"""
+
+
+def is_kind_of_class(obj, a_class):
+    """define a function"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return 

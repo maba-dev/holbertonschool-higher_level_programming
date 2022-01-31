26e088b6ff8dda7220bb95fc7c6abe7d990af892
maba-dev <cherif.ba@hotmail.fr>
2022-01-31 14:55:13 +0100

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" a function that returns the list of available attributes and methods of an object:"""
+
+
+def lookup(obj):
+    """ define a function"""
+    return dir(obj)

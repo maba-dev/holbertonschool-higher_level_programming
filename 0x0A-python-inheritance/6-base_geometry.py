8ec3f30697c0d3da2edae06c2bae2fc5fad60287
maba-dev <cherif.ba@hotmail.fr>
2022-01-31 17:43:29 +0100

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """ define a class"""
+    def area(self):
+        """ define a class """
+        raise Exception("area() is not implemented")

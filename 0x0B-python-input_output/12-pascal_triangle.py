22430ab4d78bde80d11b81f55384239259b4b5b9
maba-dev <cherif.ba@hotmail.fr>
2022-02-01 23:04:13 +0100

    task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""  a function def pascal_triangle(n): that returns a list of lists of integers"""
+
+
+def pascal_triangle(n):
+    """ define a function"""
+    list = []
+    if n <= 0:
+        return list
+    for _ in range(n):
+        row = [1]
+        if list:
+            last_row = list[-1]
+            row.extend([sum(pair) for pair in zip(last_row, last_row[1:])])
+            row.append(1)
+        list.append(row)
+    return list

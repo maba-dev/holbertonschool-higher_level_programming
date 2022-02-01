f9f15b21acd07e7e1f9a4b58c5e35a04f9580bc9
maba-dev <cherif.ba@hotmail.fr>
2022-02-01 17:03:22 +0100

    task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a function that writes an Object to a text file, using a JSON: """
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ define a function"""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
+    f.closed

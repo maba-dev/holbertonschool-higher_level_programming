8e0091eb08aae56f40df864e12c182cc92ab1878
maba-dev <cherif.ba@hotmail.fr>
2022-02-01 17:01:47 +0100

    task 4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""a function that returns an object (Python data structure) represented by a JSON string: """
+"""a function that returns an object (Python data structure) """
 import json
 
 
 def from_json_string(my_str):
+    """ define a function"""
     return json.loads(my_str)

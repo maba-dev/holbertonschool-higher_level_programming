5d84b7688fb36db8d028f250775d69a3db250839
maba-dev <cherif.ba@hotmail.fr>
2022-02-01 17:05:11 +0100

    task 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -2,6 +2,8 @@
 """a function that creates an Object from a “JSON file”: """
 import json
 
+
 def load_from_json_file(filename):
+    """ define a function"""
     with open(filename) as f:
         return json.load(f)

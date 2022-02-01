dbf30a28e56fecdc7122a3154bed2dec34533f59
maba-dev <cherif.ba@hotmail.fr>
2022-02-01 16:55:53 +0100

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""a function that writes a string to a text file (UTF8) and returns the number of characters written: """
+"""a function that writes a string to a text file (UTF8) """
+
 
 def write_file(filename="", text=""):
+    """ define a function"""
 
     with open(filename, "w", encoding="utf-8") as f:
         numb_char = f.write(text)

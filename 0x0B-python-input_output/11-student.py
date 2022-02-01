8626c56fc3551932e127a29c55f552826d21ee7c
maba-dev <cherif.ba@hotmail.fr>
2022-02-01 22:29:11 +0100

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""a class Student that defines a student by: """
+
+
+class Student:
+    """ deefine class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        dict = {}
+        for i in attrs:
+            if isinstance(i, str) and hasattr(self, i):
+                dict[i] = getattr(self, i)
+        return dict
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            self.__dict__[key] = value

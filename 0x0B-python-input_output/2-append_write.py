57d8c339fe6bfafc3d9a530363f4862517b408cd
maba-dev <cherif.ba@hotmail.fr>
2022-02-01 16:54:02 +0100

    task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""a function that appends a string at the end of a text file (UTF8) and returns the number of characters added: """
+"""a function that appends a string at the end of a text file (UTF8): """
+
 
 def append_write(filename="", text=""):
+    """ define a function"""
     with open(filename, "a+", encoding="utf-8") as f:
         numb_char = f.write(text)
     return numb_char

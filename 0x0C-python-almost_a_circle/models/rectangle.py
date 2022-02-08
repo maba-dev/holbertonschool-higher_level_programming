ed53ff49485e2e1ec80004453e2894bbdc5608ce
maba-dev <cherif.ba@hotmail.fr>
2022-02-08 14:09:31 +0100

    task 12

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -106,3 +106,10 @@ class Rectangle(Base):
             for key, value in kwargs.items():
                 if key in list_args:
                     setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ return a dictionary"""
+        dictionary = {
+            "id": self.id, "width": self.width,
+            "height": self.height, "x": self.x, "y": self.y}
+        return dict(dictionary)

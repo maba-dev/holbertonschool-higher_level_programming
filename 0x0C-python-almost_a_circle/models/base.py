da842179373f8264638a0b027b3b261a3e1c5bda
maba-dev <cherif.ba@hotmail.fr>
2022-02-08 15:17:01 +0100

    task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,7 +19,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """JSON is one of the standard formats for sharing data reprsntation"""
 
-        if list_dictionaries is None:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         else:
             return json.dumps(list_dictionaries)

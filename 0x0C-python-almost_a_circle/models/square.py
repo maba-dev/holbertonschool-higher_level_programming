f1bfb72d9b7388b72398d46a1ad1827577873840
maba-dev <cherif.ba@hotmail.fr>
2022-02-08 12:45:08 +0100

    task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,6 +5,7 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """ define a square class"""
+
     def __init__(self, size, x=0, y=0, id=None):
         """ initialize a super __init__"""
         super().__init__(size, size, x, y, id)
@@ -18,3 +19,13 @@ class Square(Rectangle):
             self.y,
             self.width
         ))
+    @property
+    def size(self):
+        """ function getter for size"""
+        return self.width
+    
+    @size.setter
+    def size(self, size):
+        self.width = size
+        self.height = size
+    

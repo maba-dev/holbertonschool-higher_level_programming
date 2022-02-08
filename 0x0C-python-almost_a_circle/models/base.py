58d375364bdf63949b549288de00b98c927b3d42
maba-dev <cherif.ba@hotmail.fr>
2022-02-08 15:05:54 +0100

    task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ the first class Base: """
 
+import json
+
 
 class Base:
     """ define a base class"""
@@ -13,3 +15,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """JSON is one of the standard formats for sharing data reprsntation"""
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)

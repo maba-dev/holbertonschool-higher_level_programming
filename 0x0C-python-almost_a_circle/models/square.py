744a791768b7c18b4456f061a4e904035c3b53d1
maba-dev <cherif.ba@hotmail.fr>
2022-02-08 10:54:19 +0100

    task 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,6 +8,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ initialize a super __init__"""
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     def __str__(self):
         """ define a function __str__()"""

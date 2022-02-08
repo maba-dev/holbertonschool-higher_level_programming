0df845d3ab24f920357e64812bad3eeb674e4fba
maba-dev <cherif.ba@hotmail.fr>
2022-02-08 10:46:27 +0100

    task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" the class Square that inherits from Rectangle: """
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """ define a square class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+    
+    def __str__(self):
+        return ("[Square] ({:d}) {:d}/{:d} - {:d}".format(
+            self.id,
+            self.x,
+            self.y,
+            self.height
+        ))

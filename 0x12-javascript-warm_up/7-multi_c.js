49402f4f5a258280ca8a0697e4cea433eb879cfc
maba-dev <cherif.ba@hotmail.fr>
2022-04-04 14:55:31 +0200

    task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv;
+const x = parseInt(args[2]);
+if (Number.isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}

e5a5b8d0726b7ac1f5ed079546a16aec714463a9
maba-dev <cherif.ba@hotmail.fr>
2022-04-04 21:42:50 +0200

    replace 'x' to 'X'

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -7,7 +7,7 @@ if (Number.isNaN(x)) {
 } else {
   for (let i = 0; i < x; i++) {
     for (let j = 0; j < x; j++) {
-      process.stdout.write('x');
+      process.stdout.write('X');
     }
     process.stdout.write('\n');
   }

d1726d54b6b020d91e538da4e9f4153698d4124c
maba-dev <cherif.ba@hotmail.fr>
2022-04-04 14:42:44 +0200

    task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const myStringArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const arrayLength = myStringArray.length;
+for (let i = 0; i < arrayLength; i++) {
+  console.log(myStringArray[i]);
+}

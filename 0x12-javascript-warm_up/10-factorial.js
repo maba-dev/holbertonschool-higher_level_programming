f079e5a0fb9315f58ace92fc1bd666b1c7a33e30
maba-dev <cherif.ba@hotmail.fr>
2022-04-04 15:59:21 +0200

    task 10: factoriel

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const args = process.argv;
+let x = parseInt(args[2]);
+if (Number.isNaN(x)) {
+  console.log('1');
+} else {
+  let result = 1;
+  while (x > 0) {
+    result = result * x;
+    x--;
+  }
+  console.log(result);
+}

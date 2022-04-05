0805a34902600a6d291a02cbef3fb0d77f56566c
maba-dev <cherif.ba@hotmail.fr>
2022-04-05 17:41:16 +0200

    task 6: Create an instance method called charPrint(c) that prints the rectangle using the character c

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        if (c !== undefined) {
+          process.stdout.write('C');
+        } else {
+          process.stdout.write('X');
+        }
+      }
+      process.stdout.write('\n');
+    }
+  }
+};

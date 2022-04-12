91fab19da9526196141e103dbe41cccfd181f97c
maba-dev <cherif.ba@hotmail.fr>
2022-04-12 23:16:53 +0000

    task 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""use the module SQLAlchemy """
+from unicodedata import name
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (create_engine)
+from sys import argv
+from sqlalchemy.orm import Session
+from model_state import State
+Base = declarative_base()
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    for state in session.query(State).where(State.name.contains('a')).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()

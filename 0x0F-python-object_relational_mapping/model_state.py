aedc15092b9165a678afe1b42d0d85a7045118a2
maba-dev <cherif.ba@hotmail.fr>
2022-04-12 12:04:38 +0000

    task 6 pycodestyle

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class SomeClass(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
-    name =  Column(String(128))
+    name = Column(String(128))

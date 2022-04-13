ace51c97dca61426a661ac0ac77b8b8da6032637
maba-dev <cherif.ba@hotmail.fr>
2022-04-13 12:48:32 +0000

    task 5 correct pycodestyle

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,7 +11,9 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    cur.execute("SELECT cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s", (argv[4],))
+    cur.execute("SELECT cities.name, states.name FROM cities JOIN \
+        states ON cities.state_id = states.id\
+         WHERE states.name LIKE %s", (argv[4],))
     result = []
     for i in cur.fetchall():
         result.append(i[0])

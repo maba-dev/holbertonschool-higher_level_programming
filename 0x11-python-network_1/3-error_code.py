d66336e8401cb01481c249313ffa4af3eea089a4
maba-dev <cherif.ba@hotmail.fr>
2022-04-22 15:21:17 +0000

    task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" manage urllib.error.HTTPError exceptions and print: Error code:"""
+
+import urllib.request
+import sys
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print('Error code:', e.code)

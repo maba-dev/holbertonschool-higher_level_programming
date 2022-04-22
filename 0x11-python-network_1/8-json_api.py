0a28841cf861fb6527ff4942d628fea93ca30706
maba-dev <cherif.ba@hotmail.fr>
2022-04-22 20:49:20 +0000

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,18 +3,18 @@
 import sys
 import requests
 
-
-if len(sys.argv) == 1:
-    q = ""
-else:
-    q = sys.argv[1]
-result = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
-
-try:
-    data = result.json()
-    if data:
-        print("[{}] {}".format(data["id"], data["name"]))
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        q = ""
     else:
-        print("No result")
-except:
-    print("Not a valid JSON")
+        q = sys.argv[1]
+    result = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
+
+    try:
+        data = result.json()
+        if data:
+            print("[{}] {}".format(data["id"], data["name"]))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")

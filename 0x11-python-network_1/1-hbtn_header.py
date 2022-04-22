5f70973b7e68f04a0852b41e1b70aa8202f0d8cf
maba-dev <cherif.ba@hotmail.fr>
2022-04-22 12:26:11 +0000

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """sends a request to the URL and displays the value of the X-Request-Id"""
-
-
 import urllib.request
 import sys
+
+
 with urllib.request.urlopen(sys.argv[1]) as response:
     header = response.info()
     print(header["X-Request-Id"])

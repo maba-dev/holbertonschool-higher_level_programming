b7a94b221b9464546ecd2d5f2eec16af3d538196
maba-dev <cherif.ba@hotmail.fr>
2022-04-22 20:01:31 +0000

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""sends a POST request to the passed URL with the email as a parameter """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    values = {'email': sys.argv[2]}
+
+    data = requests.post(values)
+    data = data.encode('ascii')
+    req = requests(url, data).text
+    print(req)

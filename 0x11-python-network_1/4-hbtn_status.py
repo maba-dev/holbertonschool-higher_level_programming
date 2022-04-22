802bc626b43c7bffc40ea77a4bb9ca3f209a6648
maba-dev <cherif.ba@hotmail.fr>
2022-04-22 19:44:41 +0000

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,8 +3,7 @@
 
 
 import requests
-with requests.get('https://intranet.hbtn.io/status') as response:
-    html = response.text
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
+html = requests.get('https://intranet.hbtn.io/status').text
+print("Body response:")
+print("\t- type: {}".format(type(html)))
+print("\t- content: {}".format(html))

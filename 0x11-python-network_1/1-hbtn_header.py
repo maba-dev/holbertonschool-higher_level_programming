079f548c028bcbda10b12f57a135cc9af6c6d318
maba-dev <cherif.ba@hotmail.fr>
2022-04-22 12:29:27 +0000

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,6 +4,7 @@ import urllib.request
 import sys
 
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    header = response.info()
-    print(header["X-Request-Id"])
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        header = response.info()
+        print(header["X-Request-Id"])

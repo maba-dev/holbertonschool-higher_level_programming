2790f6e79723ac56e347568951ca7f6c46a3448b
maba-dev <cherif.ba@hotmail.fr>
2022-04-22 12:18:47 +0000

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,7 +3,7 @@
 
 
 import urllib.request
-from sys import argv
-with urllib.request.urlopen(argv[1]) as response:
+import sys
+with urllib.request.urlopen(sys.argv[1]) as response:
     header = response.info()
     print(header["X-Request-Id"])

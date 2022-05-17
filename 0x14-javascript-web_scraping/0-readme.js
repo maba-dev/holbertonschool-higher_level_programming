12bee214da1b50332e63d110df1d5e5e823f120d
maba-dev <cherif.ba@hotmail.fr>
2022-05-17 12:59:08 +0000

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,10 +2,7 @@
 const args = process.argv;
 const fs = require('fs');
 fs.readFile(args[2],
-    {encoding:'utf-8'},
-    function(err, data) {
-        if(data)
-            console.log(data);
-        else
-            console.log(err);
-    });
+  { encoding: 'utf-8' },
+  function (err, data) {
+    if (data) { console.log(data); } else { console.log(err); }
+  });

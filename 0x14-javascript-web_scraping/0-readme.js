64844681d8c0c97ce5d5fde49830cef399f4eb19
maba-dev <cherif.ba@hotmail.fr>
2022-05-17 13:09:10 +0000

    task 0 retry

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,5 +5,5 @@ const fs = require('fs');
 fs.readFile(args[2],
   { encoding: 'utf-8' },
   function (err, data) {
-    if (data) { console.log(data); } else { console.log(err); }
+    if (err) { console.log(err); } else { console.log(data); }
   });

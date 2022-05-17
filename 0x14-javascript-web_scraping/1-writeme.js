98a36e26a6f37dc460e4d4997c44ad4b10129d12
maba-dev <cherif.ba@hotmail.fr>
2022-05-17 12:58:16 +0000

    task 1 retry

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
+
 const args = process.argv;
-const fs = require('fs')
-let data = args[3]
+const fs = require('fs');
+
+const data = args[3];
 fs.writeFile(args[2], data, (err) => {
-      
-    // In case of a error throw err.
-    if (err) throw err;
-    console.log(data);
-})
+  if (err) { throw err; } else { console.log(data); }
+});

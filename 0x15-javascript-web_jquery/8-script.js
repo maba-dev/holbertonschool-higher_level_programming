3f539277347728f05a228f268bb63c005733eaff
maba-dev <cherif.ba@hotmail.fr>
2022-05-25 21:03:33 +0000

    task 8

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+  $.each(data.results, function (index, movie) {
+    $('UL#list_movies').append('<li>' + movie.title + '</li>');
+  });
+});
